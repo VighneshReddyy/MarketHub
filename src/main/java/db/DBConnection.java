2ed278752291cc23c5fb23ca206858e9e5a65145
Vighnesh Reddy <vighneshreddysatti@gmail.com>
2026-04-04 11:31:56 +0530

    pwd change

--- src/main/java/db/DBConnection.java
+++ src/main/java/db/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
     private static final String URL = "jdbc:mysql://gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/test?sslMode=VERIFY_IDENTITY&useSSL=true&requireSSL=true";
     private static final String USER = "Bjy4mRb4vAKpxSb.root";
-    private static final String PASSWORD = "0iNTuhJP7RQVowQo";
+    private static final String PASSWORD = "0iNTuhJP7RQVowQ0";
 
     public static Connection getConnection() {
         try {
